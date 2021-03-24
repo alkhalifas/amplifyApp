b013a47943dd5177b13cf46339c96364e7562c65
Saleh Alkhalifa <salehesam@gmail.com>
2021-03-23 20:52:37 -0400

    Added Auth

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className="App">
             <header>
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1>We now have Auth Online New WebHook?!</h1>
+                <h1>We now have Auth Online New WebHook and latest?!</h1>
             </header>
             <AmplifySignOut />
         </div>
